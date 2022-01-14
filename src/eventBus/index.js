7922ddf23c3785abd872b6aa0cc9648096983b55
zhida S <chinaboy_zhida_s@sina.cn>
2022-01-15 01:46:10 +0800

    简单优化

--- src/eventBus/index.js
+++ src/eventBus/index.js
@@ -17,14 +17,14 @@ class _EventBus {
 _EventBus.prototype.myPub = function (type, ...args) {
   console.log("发布消息!" + args);
   //读取已发布的消息
-  let pubEvents = this._events.get(type);
+  let subEvents = this._events.get(type);
   //如果有
-  if (pubEvents) {
-    for (let i = 0; i < pubEvents.length; i++) {
+  if (subEvents) {
+    for (let i = 0; i < subEvents.length; i++) {
       if (args.length > 0) {
-        pubEvents[i].apply(this, args);
+        subEvents[i].apply(this, args);
       } else {
-        pubEvents[i].call(this);
+        subEvents[i].call(this);
       }
     }
   } else {
@@ -36,13 +36,13 @@ _EventBus.prototype.myPub = function (type, ...args) {
 // 订阅type类型的事件
 _EventBus.prototype.mySub = function (type, subEvent) {
   //获取type类型的处理, 如果没有, 则添加订阅
-  const pubEvents = this._events.get(type);
-  if (!pubEvents) {
+  const subEvents = this._events.get(type);
+  if (!subEvents) {
     //如果type事件没有对应的发布方法, 那么就把订阅的方法传给发布者
     //这里采用数组的形式, 如果有多个订阅者
     this._events.set(type, [subEvent]);
   } else {
-    pubEvents.push(subEvent); // 如果已有订阅者则, 直接追加
+    subEvents.push(subEvent); // 如果已有订阅者则, 直接追加
   }
   //将传入的函数返回, 用作取消订阅
   return subEvent;
@@ -51,13 +51,17 @@ _EventBus.prototype.mySub = function (type, subEvent) {
 //移除订阅
 _EventBus.prototype.removeSub = function (type, subEvent) {
   //获取对应消息的所有订阅内容
-  const pubEvents = this._events.get(type);
-  // 遍历, 找出对应的subEvent 删除
-  for (let i = 0, len = pubEvents.length; i < len; i++) {
-    if (pubEvents[i] === subEvent) {
-      pubEvents.splice(i, 1);
-      return;
+  const subEvents = this._events.get(type);
+  if (subEvents) {
+    // 遍历, 找出对应的subEvent 删除
+    for (let i = 0, len = subEvents.length; i < len; i++) {
+      if (subEvents[i] === subEvent) {
+        subEvents.splice(i, 1);
+        return;
+      }
     }
+  } else {
+    return;
   }
 };
 
